4e4eb43ebb2afb651fad7e7cea4a03857a69889f
Hussain Manasi <58601158+codesbyhussain@users.noreply.github.com>
2021-06-25 17:34:48 +0530

    Update settings.py

--- Bl_don/settings.py
+++ Bl_don/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import django_heroku
 from pathlib import Path
 import os
+import dj-database-url
+db_from_env = dj_database_url.config()
+DATABASES[‘default’].update(db_from_env)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
